aeba10ba1bc7f3e3ebb52cefa890a2d0a29c7d16
Eric <eric.schubert.3d@gmail.com>
2026-09-25 14:30:12 +0200

    Added basic python gridmap classes

---
+++ grid.py
@@ -0,0 +1,83 @@
+import pygame
+
+class DebugCell:
+    def __init__(self, x : int, y : int, cell_size : int = 50) -> None:
+        self.x = x
+        self.y = y
+        self.cell_size = cell_size
+        self.font = pygame.font.SysFont('Arial', 16)
+
+    def draw_cell(self, screen, value) -> None:
+        pos_x = self.x * self.cell_size
+        pos_y = self.y * self.cell_size
+        rect = pygame.Rect(pos_x, pos_y, self.cell_size, self.cell_size)  # init rect
+        pygame.draw.rect(screen, (200, 200, 200), rect)  # draw bg of rect
+        pygame.draw.rect(screen, (10, 10, 10), rect, 1, 1)  # draw outline of rect
+        text = self.font.render(str(value), True, (10, 10, 10))
+        text_rect = text.get_rect(center=(pos_x + self.cell_size // 2, pos_y + self.cell_size // 2))
+        screen.blit(text, text_rect)
+
+
+class Grid:
+    def __init__(self, width :int, height: int, cell_size : int = 50) -> None:
+        self.width = width
+        self.height = height
+        self.grid_list : [[int]] = [[0 for _ in range(self.width)] for _ in range(self.height)]
+        self.cell_list : [[Cell]] = [[DebugCell(x,y) for y in range(self.width)] for x in range(self.height)]
+        self.cell_size = cell_size
+
+    def get_cell_world_position(self, x :int, y :int) -> tuple:
+        return (x * self.cell_size, y * self.cell_size)
+
+    def __set_cell_value(self, pos : tuple, value :int) -> None:
+        if pos[0] >= 0 and pos[1] >= 0 and pos[0] < self.width and pos[1] < self.height:
+            self.grid_list[pos[0]][pos[1]] = value
+
+    def __get_cell(self, pos : tuple) -> tuple[int, int]:
+        return (pos[0] // self.cell_size,pos[1] // self.cell_size)
+
+    def draw_grid(self, screen) -> None:
+        for x in range(self.width):
+            for y in  range(self.height):
+                self.cell_list[x][y].draw_cell(screen, self.grid_list[x][y])
+
+    def set_cell_value(self, pos : tuple, value : int) -> None:
+        self.__set_cell_value(self.__get_cell(pos), value)
+
+
+
+def debug_grid():
+    # Initialise screen
+    pygame.init()
+    screen = pygame.display.set_mode((800, 800))
+    pygame.display.set_caption('Pygame Grid')
+
+    # Fill background
+    background = pygame.Surface(screen.get_size())
+    background = background.convert()
+    background.fill((20, 20, 20))
+
+    # Blit everything to the screen
+    screen.blit(background, (0, 0))
+    pygame.display.flip()
+
+    # Create the grid
+    grid = Grid(16, 16)
+    # Event loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        # mouse input
+        if pygame.mouse.get_just_pressed()[0]:
+            grid.set_cell_value(pygame.mouse.get_pos(), 1)
+
+        # draw the grid
+        screen.blit(background, (0, 0))
+        grid.draw_grid(screen)
+
+        pygame.display.flip()
+
+if __name__ == '__main__':
+    debug_grid()
